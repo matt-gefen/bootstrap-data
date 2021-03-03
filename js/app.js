33680ea934e316b5a74ac1848c67d2f496c2fe17
ManliestBen <benjamintmanley@gmail.com>
2021-03-03 09:54:17 -0600

    attach event listeners to buttons

--- js/app.js
+++ js/app.js
@@ -7,11 +7,19 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const swiftBtn = document.getElementById("swiftButton")
+const yeezyBtn = document.getElementById("yeezyButton")
+const container = document.getElementById("containerDiv")
 
 /*----------------------------- Event Listeners -----------------------------*/
+swiftBtn.addEventListener('click', ()=> {
+  console.log('T-Swift Rocks')
+})
 
-
+yeezyBtn.addEventListener('click', ()=> {
+  console.log('Yeezy Rocks')
+})
 
 /*-------------------------------- Functions --------------------------------*/
+
+

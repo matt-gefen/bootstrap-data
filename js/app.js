0a832a8ebe6c50e5cf726991b991b9f375d8adfa
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-06-22 22:35:26 -0500

    add quotes and helpers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+import {getRandomKanyeQuote, getRandomTaylorQuote} from "../data/quotes.js"
 
 /*-------------------------------- Variables --------------------------------*/
 
@@ -8,11 +8,19 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const swiftBtn = document.querySelector("#swift-button")
+const yeezyBtn = document.querySelector("#yeezy-button")
+const cardContainer = document.querySelector("#card-container")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+swiftBtn.addEventListener("click", () => {
+  console.log("Taylor is listening!")
+})
 
+yeezyBtn.addEventListener("click", () => {
+  console.log("Kanye is listening!")
+})
 
 /*-------------------------------- Functions --------------------------------*/
 

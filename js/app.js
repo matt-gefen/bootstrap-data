d2e6f5fc2af4579f8a386bf150288ba6a5a467b7
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-06-24 11:38:59 -0500

    start project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// - Add the HTML for two buttons (one for T-Swift, one for Yeezy)
+// - Add the HTML for a ‘Mode Swap’ or ‘Dark Mode’ button.
+// - Add a container element for the ‘card’ components to be appended to as children
+// - Add a cached element reference for the container element in the JS file
+// - Add cached element references for each of the buttons
+// - Add event listeners to each of the buttons (TEST with `console.log()`)
+// - Write code to retrieve the data and log the data using `console.log()`
+// - Tweak event listeners so that the quote is stored in a variable (TEST with `console.log()`)
+// - Create a test ‘card’ element with Bootstrap
+// - Create a render function
+// - Add a function to handle appending a ‘card’ containing the quote to the container element.
+// - Style each card differently, based on whether the quote is from T-Swift or Yeezy. (What options do we have to handle this?)
+// - Add a button to the card that will allow us to REMOVE the quote from list. (What are some approaches we can take to do this?)
+// - Add responsive Design
+// - Add Google Fonts
+// - Add Light/Dark Mode
+
 /*-------------------------------- Constants --------------------------------*/
 
 

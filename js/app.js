676eef98219514bb8f245e012c14b25d84457e18
mattgefen <matt.gefen@gmail.com>
2021-10-13 12:35:22 -0600

    bunch of functions

--- js/app.js
+++ js/app.js
@@ -13,22 +13,40 @@ const cardContainer = document.querySelector('#card-container')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-swiftButton.addEventListener("click", function(evt) {
-  const newTaylorQuote = {
-    artist: "T-Swift",
-    text: quotes.getRandomSwiftQuote()
-  }
-  newQuotes.push(newTaylorQuote)
-  console.log(newQuotes)
-})
-yeButton.addEventListener("click", function(evt) {
-  const newKanyeQuote = {
-    artist: "Kanye",
-    text: quotes.getRandomKanyeQuote()
-  }
-  newQuotes.push(newKanyeQuote)
-  console.log(newQuotes)
-})
+swiftButton.addEventListener("click", createQuote)
+yeButton.addEventListener("click", createQuote)
+
 
 
 /*-------------------------------- Functions --------------------------------*/
+function createQuote(evt) {
+  const newQuote = {
+    artist: evt.target.id === 'swift-button' ? 'T-Swift' : 'Kanye',
+    text: evt.target.id === 'swift-button' ? quotes.getRandomSwiftQuote() : quotes.getRandomKanyeQuote()
+  }
+  newQuotes.push(newQuote)
+  render()
+}
+
+function render() {
+  cardContainer.innerHTML = ''
+  newQuotes.forEach((quote) => {
+    appendQuote(quote)
+  })
+}
+
+function appendQuote(quote) {
+  let quoteCard = document.createElement("div")
+  quoteCard.classList.add("card",`${quote.artist.toLowerCase()}`)
+  quoteCard.innerHTML = 
+  `<div class="card-body">
+  <blockquote class="blockquote mb-0">
+    <p>${quote.text}</p>
+    <footer class="blockquote-footer text-end artist-name">
+    ${quote.artist}
+    </footer>
+  </blockquote>
+  </div>`
+
+  cardContainer.appendChild(quoteCard)
+}

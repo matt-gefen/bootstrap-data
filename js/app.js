7b2ced46e7b8c8fac2ac2cc1d602a9e8850566c4
mattgefen <matt.gefen@gmail.com>
2021-10-13 11:07:50 -0600

    basic sanity clicks

--- js/app.js
+++ js/app.js
@@ -7,11 +7,18 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const swiftButton = document.querySelector('#swift-button')
+const yeButton = document.querySelector('#ye-button')
+const cardContainer = document.querySelector('#card-container')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+swiftButton.addEventListener("click", function(evt) {
+  console.log('Tay Tay Yall')
+})
+yeButton.addEventListener("click", function(evt) {
+  console.log('Ye Ye Yall')
+})
 
 
 /*-------------------------------- Functions --------------------------------*/
